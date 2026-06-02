6e9b905dffe68c2970d4e68971163e2f3e510a0e
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-06-02 12:48:16 +0900

    first commit

---
+++ src/main/java/com/sh/aicommerce/AiCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.sh.aicommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh/aicommerce/AiCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.aicommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
