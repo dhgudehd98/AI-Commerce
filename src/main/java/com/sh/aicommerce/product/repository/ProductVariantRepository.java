a6079aa3447c6009753a968bc12b2c71db08d30a
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-06-19 15:38:59 +0900

    20260619_update : 상품 Entity 변경 및 추가사항 때문에 상품 등록 로직 변경

---
+++ src/main/java/com/sh/aicommerce/product/repository/ProductVariantRepository.java
@@ -0,0 +1,9 @@
+package com.sh.aicommerce.product.repository;
+
+import com.sh.aicommerce.entity.ProductVariant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductVariantRepository extends JpaRepository<ProductVariant, Long> {
+}
--- src/main/java/com/sh/aicommerce/product/service/ProductService.java
+++ src/main/java/com/sh/aicommerce/product/service/ProductService.java
@@ -1,68 +1,66 @@
 package com.sh.aicommerce.product.service;
 
+
 import com.sh.aicommerce.brand.repository.BrandRepository;
 import com.sh.aicommerce.common.exception.brand.BrandException;
 import com.sh.aicommerce.common.exception.product.ProductException;
-import com.sh.aicommerce.entity.Brand;
-import com.sh.aicommerce.entity.Product;
-import com.sh.aicommerce.entity.ProductOption;
-import com.sh.aicommerce.product.dto.ProductCreateRequestDto;
-import com.sh.aicommerce.product.dto.ProductOptionCreateRequestDto;
+import com.sh.aicommerce.entity.*;
+import com.sh.aicommerce.enums.product.ProductImageType;
+import com.sh.aicommerce.product.dto.request.ProductCreateRequestDto;
+import com.sh.aicommerce.product.dto.request.ProductImageCreateRequestDto;
+import com.sh.aicommerce.product.dto.request.ProductOptionCreateRequestDto;
+import com.sh.aicommerce.product.dto.request.ProductVariantRequestDto;
 import com.sh.aicommerce.product.dto.response.ProductCreateResponseDto;
 import com.sh.aicommerce.product.redis.ProductIndexEventRecord;
+import com.sh.aicommerce.product.repository.ProductImageRepository;
 import com.sh.aicommerce.product.repository.ProductRepository;
+import com.sh.aicommerce.product.repository.ProductVariantRepository;
 import com.sh.aicommerce.productOption.repository.ProductOptionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+
 public class ProductService {
     private final ProductRepository productRepository;
     private final ProductOptionRepository productOptionRepository;
     private final BrandRepository brandRepository;
+    private final ProductVariantRepository productVariantRepository;
+    private final ProductImageRepository productImageRepository;
+
     private final ApplicationEventPublisher eventPublisher;
+
 
     @Transactional
     public ProductCreateResponseDto createProduct(ProductCreateRequestDto createRequestDto) {
 
-        // 상품 저장
-        Brand brand = brandRepository.findById(createRequestDto.getBrandId()).orElseThrow(() -> new BrandException("해당 브랜드가 존재하지 않습니다. 브랜드를 다시 확인해주세요."));
+        Brand brand = brandRepository.findById(createRequestDto.getBrandId()).orElseThrow(() -> new BrandException("현재 존재하지 않는 브랜드입니다. 브랜드를 다시 확인해주세요."));
 
-        validateProductOption(createRequestDto.getProductOptions());
-        log.info("[상품 옵션 유효성 검사 성공]");
+        // 요청 값 유효성검사 -> 상품, 상품 Variant, 상품 옵션, 상품 이미지
+        validateProductRequest(createRequestDto.getVariants());
 
+        // 유효성 검사 통과하면 상품에 대한 데이터 저장
+        // 1. 상품 데이터 저장
         Product product = Product.create(createRequestDto, brand);
         productRepository.save(product);
-        log.info("[상품 등록 완료] 상품 ID : {} , 상품명 : {}", product.getId(), product.getProductName());
-
-        // 상품 옵션 저장
-        List<ProductOption> options = createRequestDto.getProductOptions().stream()
-                        .map(optionDto -> ProductOption.createOption(optionDto, product))
-                        .collect(Collectors.toList());
-
+        // 2. 상품 Variandant 저장
         try {
-            productOptionRepository.saveAll(options);
+            saveVariant(product, createRequestDto.getVariants());
         } catch (DataIntegrityViolationException e) {
-            throw new ProductException("이미 등록되어 있는 SKU 입니다.");
+            throw new ProductException("이미 등록된 모델번호 또는 SKU에 대한 값이 존재합니다.");
         }
 
-        log.info("[상품 옵션 저장 완료] 상품 옵션 개수 : {}", options.size());
-
-        // 상품 등록시 , 상품 색인 과정을 진행하기 위해 이벤트 처리 -> 이벤트 처리는 항상 트랜잭션 커밋 후
+        // 상품에 대한 정보를 색인하는 것이 아닌 상품 Variant 대한 부분을 색인
         eventPublisher.publishEvent(
                 new ProductIndexEventRecord(product.getId(), "CREATE")
         );
@@ -70,24 +68,97 @@ public class ProductService {
         return new ProductCreateResponseDto("Y", "상품이 성공적으로 저장되었습니다.");
     }
 
-    private void validateProductOption(List<ProductOptionCreateRequestDto> options) {
+    private void saveVariant(Product product, List<ProductVariantRequestDto> variants) {
 
-        log.info("[상품 옵션 유효성 검사 시작]");
-        if (options == null || options.isEmpty()) throw new ProductException("상품 옵션을 한개 이상 등록해야 합니다.");
+        for (ProductVariantRequestDto variantRequestDto : variants) {
+
+            //상품 Variant 저장
+            ProductVariant variant = ProductVariant.create(product, variantRequestDto);
+            productVariantRepository.save(variant);
+
+            saveProductImages(variant, variantRequestDto.getImages());
+            saveProductOptions(variant, variantRequestDto.getProductOptions());
+        }
+    }
+
+    private void saveProductImages(ProductVariant variant, List<ProductImageCreateRequestDto> dtoImages) {
+        List<ProductImage> images = dtoImages.stream()
+                .map(imageDto -> ProductImage.create(variant, imageDto))
+                .toList();
+
+        productImageRepository.saveAll(images);
+    }
+    private void saveProductOptions(ProductVariant variant, List<ProductOptionCreateRequestDto> optionDtos) {
+        List<ProductOption> options = optionDtos.stream()
+                .map(optionDto -> ProductOption.createOption(variant, optionDto))
+                .toList();
+
+        productOptionRepository.saveAll(options);
+    }
+
+
+    private void validateProductRequest(List<ProductVariantRequestDto> variants) {
+
+        if(variants == null || variants.isEmpty()) throw new ProductException("상품 Variant에 대한 값을 하나 이상 등록해야합니다.");
 
         Set<String> skus = new HashSet<>();
-        Set<String> optionCombinations = new HashSet<>();
+        Set<String> models = new HashSet<>();
+        Set<String> colors = new HashSet<>();
 
-        for (ProductOptionCreateRequestDto optionDto : options) {
-            String sku = optionDto.getSku();
-            String combination = optionDto.getColor() + ":" + optionDto.getSize();
+        for (ProductVariantRequestDto variantRequestDto : variants) {
+            validateVariantRequestDto(variantRequestDto, skus, models, colors);
+        }
 
-            if(!skus.add(sku)) throw new ProductException("요청에 중복된 sku가 존재합니다. sku : " + sku);
-            if(!optionCombinations.add(combination))
-                throw new ProductException("동일한 색상과 사이즈가 존재합니다. 옵션 : " + combination);
 
-            // DB에 동일한 sku가 존재하는지 확인
-            if(productOptionRepository.existsBySku(sku)) throw new ProductException("이미 등록되어 있는 SKU 입니다. SKU : " + sku);
+    }
+
+    private void validateVariantRequestDto(ProductVariantRequestDto variantRequestDto, Set<String> skus, Set<String> models, Set<String> colors) {
+
+        if(!models.add(variantRequestDto.getModelNumber()))
+            throw new ProductException("중복된 모델번호가 존재합니다. 모델번호 : " + variantRequestDto.getModelNumber());
+
+        if (!colors.add(variantRequestDto.getColor())) {
+            throw new ProductException("중복된 색상이 존재합니다. 색상 : " + variantRequestDto.getColor());
+        }
+
+        // 이미지 유효성 검사
+        validateProductImageRequestDto(variantRequestDto);
+
+        // 상품 옵션 유효성 검사
+        validateProductOptionRequestDto(variantRequestDto.getProductOptions(), skus);
+
+    }
+
+    private void validateProductImageRequestDto(ProductVariantRequestDto variantRequestDto) {
+
+        // 이미지 순서
+        Set<Integer> orders = new HashSet<>();
+
+        // 썸네일 이미지는 하나만 등록하는 유효성 검사
+        long thumbNailImageCount = variantRequestDto.getImages().stream()
+                .filter(productImageCreateRequestDto -> productImageCreateRequestDto.getImageType() == ProductImageType.THUMBNAIL)
+                .count();
+
+        if(thumbNailImageCount != 1 ) throw new ProductException("썸네일 이미지는 하나만 등록할 수 있습니다.");
+
+        for (ProductImageCreateRequestDto imageDto : variantRequestDto.getImages()) {
+            if(!orders.add(imageDto.getDisplayOrder()))
+                throw new ProductException(variantRequestDto.getVariantName() + " 에 중복된 이미지 표시 순서가 존재합니다.");
+        }
+    }
+
+    // 상품 옵션 유효성 검사
+    private void validateProductOptionRequestDto(List<ProductOptionCreateRequestDto> productOptions, Set<String> skus) {
+        Set<String> sizes = new HashSet<>();
+        for (ProductOptionCreateRequestDto optionDto : productOptions) {
+            if(!skus.add(optionDto.getSku()))
+                throw new ProductException("동일한 SKU에 대한 값이 존재합니다. SKU 값 : " + optionDto.getSku());
+
+            if(!sizes.add(optionDto.getSize()))
+                throw new ProductException("중복된 사이즈 값이 있습니다. SIZE : " + optionDto.getSize());
+
+            if(productOptionRepository.existsBySku(optionDto.getSku()))
+                throw new ProductException("이미 등록된 SKU 입니다. SKU : " + optionDto.getSku());
         }
     }
 
