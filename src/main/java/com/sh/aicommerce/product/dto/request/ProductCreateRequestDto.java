178baab2bf8610c1899d778b816e478c2bc414f9
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-06-19 15:38:18 +0900

    20260619_update : 상품 등록 요청 DTO 값 변경 / 상품 이미지 RequestDto , 상품 Variant RequestDto , 상품 RequestDto, 상품 옵션 RequestDto 생성 및 수정

--- src/main/java/com/sh/aicommerce/product/dto/request/ProductCreateRequestDto.java
+++ src/main/java/com/sh/aicommerce/product/dto/request/ProductCreateRequestDto.java
@@ -1,11 +1,10 @@
-package com.sh.aicommerce.product.dto;
+package com.sh.aicommerce.product.dto.request;
 
 import com.sh.aicommerce.enums.product.ProductCategory;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,18 +21,14 @@ public class ProductCreateRequestDto {
     private Long brandId;
 
     @NotBlank
-    private String productName;
+    private String baseProductName;
 
     @NotNull(message = "상품 카테고리는 필수입니다.")
     private ProductCategory productCategory;
-
-    @NotNull
-    @Positive
-    private Integer productPrice;
 
     @NotBlank
     private String productDescription;
 
     @NotEmpty
-    private List<@Valid ProductOptionCreateRequestDto> productOptions;
+    private List<@Valid ProductVariantRequestDto> variants;
 }
