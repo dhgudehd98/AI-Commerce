3322d0f674c97edc83f72efb14260d088c4bfc27
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-07-12 00:30:19 +0900

    20260712_update : Delivery Entity 배송회사 컬럼 추가

--- src/main/java/com/sh/aicommerce/enums/delivery/DeliveryStatus.java
+++ src/main/java/com/sh/aicommerce/enums/delivery/DeliveryStatus.java
@@ -1,9 +1,9 @@
 package com.sh.aicommerce.enums.delivery;
 
 public enum DeliveryStatus {
-    READY,
-    PREPARING,
-    SHIPPED,
-    DELIVERED,
-    CANCELED
+    CREATE, // 배송 생성
+    PREPARING, // 배송준비
+    SHIPPED, // 상품 배송중
+    DELIVERED, // 상품 배달 완료
+    CANCELED // 주문 취소
 }
