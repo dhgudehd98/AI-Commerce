8c53edaffa18f7f43561ed2221d22944a11292b3
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-07-16 01:18:59 +0900

    20260714_update : [Toss Payment] Toss Payment 결제 내역 저장 TossPayment Entity 추가

---
+++ src/main/java/com/sh/aicommerce/toss/repository/TossPaymentLogRepository.java
@@ -0,0 +1,7 @@
+package com.sh.aicommerce.toss.repository;
+
+import com.sh.aicommerce.entity.TossPaymentLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TossPaymentLogRepository extends JpaRepository<TossPaymentLog, Long> {
+}
