bf4c4babb610c28b458265118fc4ac05a6a382b4
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-07-22 20:40:21 +0900

    20260722_update : [토스페이먼츠 결제 위젯] 결제 위젯 로직 순서 변경 실제 결제가 이루어지는 지점 위치 변경

--- src/main/java/com/sh/aicommerce/order/orderRepository/OrderRepository.java
+++ src/main/java/com/sh/aicommerce/order/orderRepository/OrderRepository.java
@@ -2,9 +2,23 @@ package com.sh.aicommerce.order.orderRepository;
 
 import com.sh.aicommerce.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface OrderRepository extends JpaRepository<Orders, Long> {
+
+    @Query(
+    """
+    select o
+    from Orders o
+    join fetch o.payment p
+    where
+        o.orderNumber = :orderNumber
+    """)
+    Optional<Orders> findByMemberIdAndOrderNumberWithPayment(@Param("orderNumber") String orderNumber);
 }
