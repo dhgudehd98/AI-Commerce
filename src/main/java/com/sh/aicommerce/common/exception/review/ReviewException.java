ed1b183b670a08c3605d8da60e82683ab1d2181c
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-07-29 16:51:46 +0900

    20260729_update : Review Entity 변경

---
+++ src/main/java/com/sh/aicommerce/common/exception/review/ReviewException.java
@@ -0,0 +1,8 @@
+package com.sh.aicommerce.common.exception.review;
+
+public class ReviewException extends RuntimeException {
+
+    public ReviewException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/sh/aicommerce/enums/review/FitEvaluation.java
@@ -0,0 +1,7 @@
+package com.sh.aicommerce.enums.review;
+
+public enum FitEvaluation {
+    SMALL,
+    TRUE_TO_SIZE,
+    LARGE
+}
